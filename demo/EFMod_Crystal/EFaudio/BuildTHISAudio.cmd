e01baa60b346873680af16385ba49a65dbdb0696
Scott Silliman <scott.t.silliman@gmail.com>
2020-03-24 20:13:49 -0400

    zap

--- demo/EFMod_Crystal/EFaudio/BuildTHISAudio.cmd
+++
@@ -1,3 +0,0 @@
-REM This IS a simple test of nodejs load and launch 
-
-node ../../EFTutorBuilder/AudioBuilder/dist/builder.js MODULETHIS
--- demo/EFTutors/EFTED_RQSelect/EFdata/BuildTHISData.cmd
+++
@@ -1,3 +0,0 @@
-REM This IS a simple test of nodejs load and launch 
-
-node ../../../EFTutorBuilder/DataBuilder/dist/builder.js GLOBALTHIS
